b6a4f804893eb6c23b412413773fcfca5bc9acbd
mahbubul14 <mahbub21alam2017@gmail.com>
2021-12-16 07:38:07 +0600

    Fix linters error

--- main.rb
+++ main.rb
@@ -1,42 +1,20 @@
-def decode_char (str)
+def decode_char(str)
   morse = {
-    '.-' => 'A',
-    '-...' => 'B',
-    '-.-.' => 'C',
-    '-..' => 'D',
-    '.' => 'E',
-    '..-.' => 'F',
-    '--.' => 'G',
-    '....' => 'H',
-    '..' => 'I',
-    '.---' => 'J',
-    '-.-' => 'K',
-    '.-..' => 'L',
-    '--' => 'M',
-    '-.' => 'N',
-    '---' => 'O',
-    '.--.' => 'P',
-    '--.-' => 'Q',
-    '.-.' => 'R',
-    '...' => 'S',
-    '-' => 'T',
-    '..-' => 'U',
-    '...-' => 'V',
-    '.--' => 'W',
-    '-..-' => 'X',
-    '-.--' => 'Y',
-    '--..' => 'Z'
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+    '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
+    '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+    '-.--' => 'Y', '--..' => 'Z'
   }
   morse[str]
 end
 
 def decode_word(str)
-  words = str.split.map {|word| decode_char(word)}
+  words = str.split.map { |word| decode_char(word) }
   words.join
 end
 
 # def decode(str)
-#   messages = str.split('   ').map {|message| decode_word(message)}
+#   messages = str.split('   ').map { |message| decode_word(message) }
 #   messages.join(' ')
 # end
 
